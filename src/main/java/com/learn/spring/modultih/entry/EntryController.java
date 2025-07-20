51540f028783342c43630b6f8386a3ed6bf80090
VaibhavKVerma <vaibhavverma15062001@gmail.com>
2025-07-21 01:08:16 +0530

    Controller

--- src/main/java/com/learn/spring/modultih/entry/EntryController.java
+++ src/main/java/com/learn/spring/modultih/entry/EntryController.java
@@ -1,4 +1,21 @@
 package com.learn.spring.modultih.entry;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/entry")
 public class EntryController {
+    private final EntryService entryService;
+
+    @PostMapping("/")
+    ResponseEntity<ParkingEntry> createEntry(String vehicleNumber) {
+        entryService.vehicleEntry(vehicleNumber);
+        return ResponseEntity.accepted().build();
+    }
+
 }
